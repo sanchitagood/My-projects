dc21c8e9fd615aaaf9d7053f480ef919a51b3a6f
sanchitagood <106522986+sanchitagood@users.noreply.github.com>
2022-06-13 17:31:34 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+from msilib.schema import ODBCDataSource
+from re import A
+
+
+print("hello world")
+
+username=input ("Enter name")
+password=input ("Enterpassword")
+print(username)
+print (password)
+print("You will turn 100 years old in 2100")
+num= int(input ("A"))
+mod=num%2
+if (mod>0):
+    print("num is odd")
+else:
+    print("num is even")
---
+++ mltiplication.py
@@ -0,0 +1,7 @@
+from re import I
+
+
+a=int(input("enter number for multiplication"))
+i=1
+for i in range(1,11):
+    print (a,'*',i,'=', a * i)
